464250324da240e043d4fb6f7c954fed3d1debe6
nkleemann <n.kleemann@posteo.de>
2019-07-01 21:28:40 +0200

    Begin adding .compose file support

---
+++ amsel-core/Compose/Parse.swift
@@ -0,0 +1,21 @@
+/**
+ Parse .compose files.
+ 
+ A .compose can look like this:
+ 
+ Calm
+ loop, cycles: 8
+ A  major, bars: 4
+ E  minor, bars: 2
+ F  minor, bars: 2
+ G# major, bars: 4
+ end
+ 
+ Keywords:
+ Control Flow:
+ 'loop',
+ 'end'
+ Time:
+ 'cycles',
+ 'bars'
+ */
