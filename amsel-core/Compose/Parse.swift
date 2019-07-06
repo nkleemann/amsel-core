8fde8943f5bae6843ec18092ad4d811819761bb9
nkleemann <n.kleemann@posteo.de>
2019-07-06 23:35:06 +0200

    Fix indention

--- amsel-core/Compose/Parse.swift
+++ amsel-core/Compose/Parse.swift
@@ -4,18 +4,19 @@
  A .compose can look like this:
  
  Calm
+ 
  loop, cycles: 8
- A  major, bars: 4
- E  minor, bars: 2
- F  minor, bars: 2
- G# major, bars: 4
+    A  major, bars: 4
+    E  minor, bars: 2
+    F  minor, bars: 2
+    G# major, bars: 4
  end
  
  Keywords:
- Control Flow:
- 'loop',
- 'end'
- Time:
- 'cycles',
- 'bars'
+    Control Flow:
+        'loop',
+        'end'
+    Time:
+        'cycles',
+        'bars'
  */
